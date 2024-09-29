cee0aeb9f81c2cddb0fb0c81be49fdeefeccdb43
Ryan Carroll <ryan8440@gmail.com>
2024-09-29 19:28:54 -0400

    add projects section

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -14,11 +14,32 @@ export default function Projects() {
 
   return (
     <section className="h-screen items-center justify-center mt-4">
-        <Carousel responsive={responsive} infinite={true} itemClass={styles.carouselItems}>
-          <div>Item 1</div>
-          <div>Item 2</div>
-          <div>Item 3</div>
-          <div>Item 4</div>
+        <h1 className="text-2xl font-bold text-white pb-7">What I've done</h1>
+        <Carousel responsive={responsive} infinite={true} itemClass={styles.carouselItems} autoPlay={true} autoPlaySpeed={5000}>
+          <div>
+            <a href="https://apps.apple.com/us/app/travel-bingo-road-trip-bingo/id6479892388" target='_blank'>
+              <label htmlFor='travel-bingo' className='mb-2 text-lg'>Travel Bingo Mobile App</label>
+              <img id='travel-bingo' src='images/portfolio/bingo-ad.jpeg' alt='travel bingo game' />
+            </a>
+          </div>
+          <div>
+          <a href="https://smallgroup.carrollmedia.dev/reading-tracker/" target='_blank'>
+            <label htmlFor='reading-time' className='mb-2 text-lg'>Reading Time Analytics</label>
+              <img id='reading-time' src='images/portfolio/dash.png' alt='reading dashboard' />
+          </a>
+          </div>
+          <div>
+            <a href="http://data.carrollmedia.dev/public-dashboards/c9a3620259a841a4afd1ed97c3c333c0" target='_blank'>
+              <label htmlFor='environmental' className='mb-2 text-lg'>Environmental Noise Dashboard</label>
+              <img id='environmental' src='images/portfolio/db.png' alt='environmental noise dashboard' />
+            </a>
+          </div>
+          <div>
+            <a href="https://eubankselectric.com/">
+              <label htmlFor='eubanks' className='mb-2 text-lg'>Eubanks Electric Redesign</label>
+              <img id='eubanks' src='images/portfolio/eubanks.JPG' alt='Eubanks Electric' />
+            </a>
+          </div>
         </Carousel>
     </section>
   );
